579ffc9d62162c8fbb1d417100e9b05165af2a07
ClausClaus <weixinla84@gmail.com>
2020-04-12 18:59:27 +0800

    initial commit

---
+++ server/index.js
@@ -0,0 +1,55 @@
+var express = require('express'),
+  nodeExcel = require('excel-export'),
+  app = express();
+
+app.get('/api/Excel', function (req, res) {
+  var conf = {};
+  conf.cols = [
+    {
+      caption: 'string',
+      captionStyleIndex: 1,
+      type: 'string',
+      beforeCellWrite: function (row, cellData) {
+        return cellData.toUpperCase();
+      },
+      width: 15,
+    },
+    {
+      caption: 'date',
+      type: 'date',
+      beforeCellWrite: (function () {
+        var originDate = new Date(Date.UTC(1899, 11, 30));
+        return function (row, cellData, eOpt) {
+          if (cellData === null) {
+            eOpt.cellType = 'string';
+            return 'N/A';
+          } else return (cellData - originDate) / (24 * 60 * 60 * 1000);
+        };
+      })(),
+      width: 20.85,
+    },
+    {
+      caption: 'bool',
+      type: 'bool',
+    },
+    {
+      caption: 'number',
+      type: 'number',
+      width: 30,
+    },
+  ];
+  conf.rows = [
+    ['pi', new Date(Date.UTC(2013, 4, 1)), true, 3.14159],
+    ['e', new Date(2012, 4, 1), false, 2.7182],
+    ["M&M<>'", new Date(Date.UTC(2013, 6, 9)), false, 1.61803],
+    ['null date', null, true, 1.414],
+  ];
+  var result = nodeExcel.execute(conf);
+  res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+  res.setHeader('Content-Disposition', 'attachment; filename=' + 'Report.xlsx');
+  res.end(result, 'binary');
+});
+
+app.listen(3001, () => {
+  console.log('http://localhost:3001/api/Excel');
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const exportScheme = () => {
+    axios('/api/Excel', {
+      responseType: 'blob',
+    })
+      .then((res) => {
+        console.log('res:', res);
+        const blob = new Blob([res.data], {
+          type: res.data.type,
+        });
+        const fileName = 'xxx.xlsx';
+        const linkNode = document.createElement('a');
+
+        linkNode.download = fileName;
+        linkNode.style.display = 'none';
+        linkNode.href = URL.createObjectURL(blob);
+        document.body.appendChild(linkNode);
+        linkNode.click();
+
+        URL.revokeObjectURL(linkNode.href);
+        document.body.removeChild(linkNode);
+      })
+      .catch((err) => {
+        console.log('err:', err);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={exportScheme}>导出</button>
     </div>
   );
 }
